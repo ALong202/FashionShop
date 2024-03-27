4c9b2df4ab56c1d1ebaeb5f9ac7596f17eb8f8f2
DR12719 <ngocanhnguyen0801@gmail.com>
2024-03-27 23:12:36 +0700

    Update data.js, Create data seeder.js

--- backend/seeder/data.js
+++ backend/seeder/data.js
@@ -1,3 +1,242 @@
 export default [
+  
+  //1...................................... 
+  {
+    name: "ÁO SƠ MI - AR230315DT",
+    price: 580000,
+    description:
+      " Áo sơ mi dài tay, kiểu dáng Regular Fit dễ mặc, hợp form dáng. Màu sắc và kiểu dáng trẻ trung, kiểu dáng hiện đại, dễ phối đồ.",
+    ratings: 4.5,
+    images: [
+      {
+        public_id: "yst8iCm",
+        url: "https://imgur.com/yst8iCm",
+      },
+      {
+        public_id: "3yOTMCw",
+        url: "https://imgur.com/3yOTMCw",
+      },
+      {
+        public_id: "EX3Bi7p",
+        url: "https://imgur.com/EX3Bi7p",
+      },
+    ],
+    category: "Áo",
+    subCategory: "Áo sơ mi",
+    color: "Trắng",
+    size: ["S","M", "L"], 
+    stock: 50,
+    numOfReviews: 32,
+    reviews: [],
+  },
 
-  ];
+  //2..........................................
+  {
+    name: "Áo Polo - AP252315D",
+    price: 790000,
+    description:
+      "Áo Polo kiểu dáng body fit, tôn dáng người mặc. Màu sắc nổi bật, dễ phối đồ.",
+    ratings: 1.65,
+    images: [
+      {
+        public_id: "7cZ4HsS",
+        url: "https://imgur.com/7cZ4HsS",
+      },
+      {
+        public_id: "Ht9kRPl",
+        url: "https://imgur.com/Ht9kRPl",
+      },
+      {
+        public_id: "5r2ou8L",
+        url: "https://imgur.com/5r2ou8L",
+      }
+    ],
+    category: "Áo",
+    subCategory: "Áo Polo",
+    color: "Đỏ",
+    size: ["S","M", "L"], 
+    stock: 0,
+    numOfReviews: 2,
+    reviews: [],
+  },
+
+  //3.  
+  {
+    name: "ÁO T-SHIRT - TSN231441",
+    price: 370000,
+    description:
+      "Áo T-shirt chất liệu: COTTON MODAL. Màu sắc trẻ trung, dễ phối đồ. Kiểu dáng body fit tôn dáng.",
+    ratings: 3.5,
+    images: [
+      {
+        public_id: "485qQER",
+        url: "https://imgur.com/485qQER",
+      },
+      {
+        public_id: "S8JCAWE",
+        url: "https://imgur.com/S8JCAWE",
+      },
+      {
+        public_id: "ebvuGDB",
+        url: "https://imgur.com/ebvuGDB",
+      }
+    ],
+    category: "Áo",
+    subCategory: "Áo T-shirt",
+    color: "Xanh",
+    size: ["S","M", "L"], 
+    stock: 1,
+    numOfReviews: 12,
+    reviews: [],
+  },
+
+  //4.
+  {
+    name: "ÁO LEN - ALD231656",
+    price: 545000,
+    description:
+      "Áo len dáng regular fit tôn dáng người mặc. Màu sắc thanh lịch dễ phối đồ.",
+    ratings: 4.5,
+    images: [
+      {
+        public_id: "ivttBn1",
+        url: "https://imgur.com/ivttBn1",
+      },
+      {
+        public_id: "Xwozhxr",
+        url: "https://imgur.com/Xwozhxr",
+      },
+      {
+        public_id: "IUPF0sF",
+        url: "https://imgur.com/IUPF0sF",
+      },
+    ],
+    category: "Áo",
+    subCategory: "Áo len",
+    color: "Đỏ",
+    size: ["S","M"], 
+    stock: 11,
+    numOfReviews: 112,
+    reviews: [],
+  },
+
+    //5. 
+  {
+    name: "QUẦN JEANS - QJS230161",
+    price: 625000,
+    description:
+      "Quần jeans dáng slimfit ôm dáng người mặc. Màu sắc trẻ trung dễ phối đồ.",
+    ratings: 4.8,
+    images: [
+      {
+        public_id: "jjPXifI",
+        url: "https://imgur.com/jjPXifI",
+      },
+      {
+        public_id: "4W6323b",
+        url: "https://imgur.com/4W6323b",
+      },
+    ],
+    category: "Quần",
+    subCategory: "Quần Jeans",
+    color: "Xanh",
+    size: ["M", "L"], 
+    stock: 122,
+    numOfReviews: 2671,
+    reviews: [],
+  },
+
+  //6.
+  {
+    name: "QUẦN TÂY - QS231535",
+    price: 699000,
+    description:
+      "",
+    ratings: 4.1,
+    images: [
+      {
+        public_id: "DJrhCh7",
+        url: "https://imgur.com/DJrhCh7",
+      },
+      {
+        public_id: "62h2dhh",
+        url: "https://imgur.com/62h2dhh",
+      },
+    ],
+    category: "Quần",
+    subCategory: "Quần tây",
+    color: "Be",
+    size: ["S","M", "L"], 
+    stock: 1123,
+    numOfReviews: 6,
+    reviews: [],
+  },
+
+  //7.
+  {
+    name: "THẮT LƯNG - BELT232615",
+    price: 799000,
+    description: 
+      "Chất liệu: Leather (Da)",
+    ratings: 4.7,
+    images: [
+      {
+        public_id: "D4bsMSN",
+        url: "https://imgur.com/D4bsMSN",
+      },
+    ],
+    category: "Phụ kiện",
+    subCategory: "Thắt lưng",
+    color: "Đen",
+    size: "F", 
+    stock: 131,
+    numOfReviews: 222,
+    reviews: [],
+  },
+
+  //8.
+  {
+    name: "CÀ VẠT - CV221500",
+    price: 499000,
+    description:
+      "",
+    ratings: 4.9,
+    images: [
+      {
+        public_id: "ZDgWQYW",
+        url: "https://imgur.com/ZDgWQYW",
+      },
+     
+    ],
+    category: "Phụ kiện",
+    subCategory: "Cà vạt",
+    color: "Xanh",
+    size: "F",
+    stock: 0,
+    numOfReviews: 56,
+    reviews: [],
+  },
+
+  //9.
+  {
+    name: "TẤT - TA221475",
+    price: 39000,
+    description:
+      "Chất liệu Cotton co giãn thoải mái thoáng khí. Thiết kế Basic hiện đại, trẻ trung",
+    ratings: 5,
+    images: [
+      {
+        public_id: "DvnzzGn",
+        url: "https://imgur.com/DvnzzGn",
+      },
+      
+    ],
+    category: "Phụ kiện",
+    subCategory: "Tất",
+    color: "Đen",
+    size: ["S","M", "L"], 
+    stock: 500,
+    numOfReviews: 1,
+    reviews: [],
+  },
+];
