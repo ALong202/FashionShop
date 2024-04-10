ebbb6a0eee3b254dfd22bec5a8cea91691fb203a
ricardotran92 <quanghung.850621@gmail.com>
2024-04-10 17:05:20 +0700

    Hiển thị các sản phẩm trên Homepage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// ref: https://react-redux.js.org/api/provider  provide the store to the rest of the app
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
@@ -15,3 +21,4 @@ root.render(
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
