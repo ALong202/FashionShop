1289d62a61a5c8037bc817f561a8b42ae7f72f37
Pham Tien Nam <tiennam27@gmail.com>
2024-04-10 10:45:42 +0700

    add ApiFilter

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,16 +1,43 @@
+import { query } from "express";
 import Product from "../models/product.js"; // Import model Product từ đường dẫn ../models/product.js
+import APIFilters from "../utils/apiFilters.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
 /*
 Hàm điều khiển (controller functions) cho các file routes và xác định route
 Các điều khiển và các logic cho tài nguyên sản phẩm (product resource)
 */
-export const getProducts = async (req, res) => { // Khai báo hàm điều khiển getProducts nhận req và res làm tham số
-    const products = await Product.find(); // Tìm tất cả các sản phẩm từ cơ sở dữ liệu và gán cho biến products
-    res.status(200).json({ // Trả về mã trạng thái 200 và dữ liệu JSON chứa danh sách sản phẩm
-        products, // Trả về danh sách sản phẩm
-    }); 
+
+/*
+export const getProducts = async (req, res) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    products,
+  });
 };
+
+
+*/
+
+
+
+export const getProducts = async (req, res) => { // Khai báo hàm điều khiển getProducts nhận req và res làm tham số
+
+  const apiFilters = new APIFilters(Product, req.query).search();
+
+  let products = await apiFilters.query;    
+  let filteredProductsCount = products.length
+   
+  res.status(200).json({// Trả về mã trạng thái 200 và dữ liệu JSON chứa danh sách sản phẩm
+      filteredProductsCount, 
+      products, // Trả về danh sách sản phẩm
+  }); 
+};
+
+
+
+
 
 //Tạo sản phẩm mới với đường dẫn => /api/admin/products
 export const newProduct = async (req, res) => { // Khai báo hàm điều khiển newProduct nhận req và res làm tham số
@@ -26,7 +53,7 @@ export const getProductDetail = async (req, res) => { // Khai báo hàm điều 
     const product = await Product.findById(req?.params?.id ); // Tạo một sản phẩm mới từ dữ liệu được gửi trong yêu cầu và gán cho biến product
 
     if(!product) {
-        return next(new ErrorHandler("Không tìm thấy sản phẩm", 404));   
+        return next(new ErrorHandler("Không tìm thấy sản phẩm", 404));   //sử dụng một instance của lớp ErrorHandler và gọi hàm next để trả về lỗi 404
     }
 
     res.status(200).json({ // Trả về mã trạng thái 200 và dữ liệu JSON chứa thông tin sản phẩm mới được tạo
@@ -36,10 +63,10 @@ export const getProductDetail = async (req, res) => { // Khai báo hàm điều 
 
 //Update chi tiết sản phẩm mới với đường dẫn => /products/:id
 export const updateProduct = async (req, res) => { // Khai báo hàm điều khiển newProduct nhận req và res làm tham số
-    let product = await Product.findById(req?.params?.id ); // Tạo một sản phẩm mới từ dữ liệu được gửi trong yêu cầu và gán cho biến product
+    let product = await Product.findById(req?.params?.id ); // Tìm kiếm sản phẩm: sử dụng phương thức findById của Mongoose để tìm kiếm sản phẩm với ID được cung cấp trong yêu cầu (req.params.id).
 
-    if(!product) {      
-        return next(new ErrorHandler("Không tìm thấy sản phẩm", 404));   
+    if(!product) {       //Kiểm tra sự tồn tại của sản phẩm:
+        return next(new ErrorHandler("Không tìm thấy sản phẩm", 404));   //sử dụng một instance của lớp ErrorHandler và gọi hàm next để trả về lỗi 404
     }
 
     product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
@@ -56,17 +83,17 @@ export const updateProduct = async (req, res) => { // Khai báo hàm điều khi
 
 //Xóa sản phẩm với đường dẫn => /products/:id
 export const deleteProduct = async (req, res, next) => { // Khai báo hàm điều khiển newProduct nhận req và res làm tham số
-  const product = await Product.findById(req?.params?.id ); // Tạo một sản phẩm mới từ dữ liệu được gửi trong yêu cầu và gán cho biến product
+  const product = await Product.findById(req?.params?.id ); // Tìm kiếm sản phẩm: sử dụng phương thức findById của Mongoose để tìm kiếm sản phẩm với ID được cung cấp trong yêu cầu (req.params.id).
 
   if(!product) {
     //throw new error()
-      return next(new ErrorHandler("Không tìm thấy sản phẩm", 404));   
+      return next(new ErrorHandler("Không tìm thấy sản phẩm", 404));   //sử dụng một instance của lớp ErrorHandler và gọi hàm next để trả về lỗi 404
   }
 
-  await product.deleteOne();
+  await product.deleteOne(); //Nếu sản phẩm tồn tại, sử dụng phương thức deleteOne để xóa sản phẩm khỏi cơ sở dữ liệu.
 
 
-  res.status(200).json({ // Trả về mã trạng thái 200 và dữ liệu JSON chứa thông tin sản phẩm mới được tạo
+  res.status(200).json({ // Trả về mã trạng thái 200 và dữ liệu JSON chứa thông tin sản phẩm mới được xóa
       message: "Đã xóa sản phẩm",
   });
 };
