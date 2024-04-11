9e6803e839755e4c3c9759ec317ee41da0d62219
Pham Tien Nam <tiennam27@gmail.com>
2024-04-11 10:29:47 +0700

    update products filters

--- backend/utils/apiFilters.js
+++ backend/utils/apiFilters.js
@@ -1,3 +1,5 @@
+import { json } from "express";
+
 class APIFilters{
     constructor(query, queryStr){
         this.query = query;
@@ -15,6 +17,22 @@ class APIFilters{
 
         this.query = this.query.find({ ...keyword});
         return this;
+
+    }
+
+    filters() {
+        const queryCopy = { ...this.queryStr};
+
+        const fieldsToRemove = ["keyword"];
+
+        fieldsToRemove.forEach((el) => delete queryCopy[el]);
+
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+
 
     }
 
