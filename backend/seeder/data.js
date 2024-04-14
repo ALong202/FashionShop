c4f38174756a17769122decf71e22ec1189dee16
DR12719 <ngocanhnguyen0801@gmail.com>
2024-04-14 07:27:54 +0700

    modified data.js

--- backend/seeder/data.js
+++ backend/seeder/data.js
@@ -31,7 +31,6 @@ export default [
         }
       }
     },
-    // subCategory: "Áo sơ mi",
     color: "Trắng",
     size: ["S","M", "L"], 
     stock: 50,
@@ -40,6 +39,315 @@ export default [
   },
 
  
+ //2..........................................
+ {
+  productID: "AP252315D",
+  name: "Áo Polo - AP252315D",
+  price: 790000,
+  description:
+    "Áo Polo kiểu dáng body fit, tôn dáng người mặc. Màu sắc nổi bật, dễ phối đồ.",
+  ratings: 1.65,
+  images: [
+    {
+      public_id: "7cZ4HsS",
+      url: "https://i.imgur.com/7cZ4HsS.png",
+    },
+    {
+      public_id: "Ht9kRPl",
+      url: "https://i.imgur.com/Ht9kRPl.png",
+    },
+    {
+      public_id: "5r2ou8L",
+      url: "https://i.imgur.com/Ht9kRPl.png",
+    }
+  ],
+  category: {
+    "name": "Nam",
+    "subCategory": {
+      "name": "Áo",
+      "subSubCategory": {
+        "name": "Áo Polo"
+      }
+    }
+  },
+  color: "Đỏ",
+  size: ["S","M", "L"], 
+  stock: 0,
+  numOfReviews: 2,
+  reviews: [],
+},
+
+//3......................................... 
+{
+  productID: "TSN231441",
+  name: "ÁO T-SHIRT - TSN231441",
+  price: 370000,
+  description:
+    "Áo T-shirt chất liệu: COTTON MODAL. Màu sắc trẻ trung, dễ phối đồ. Kiểu dáng body fit tôn dáng.",
+  ratings: 3.5,
+  images: [
+    {
+      public_id: "485qQER",
+      url: "https://i.imgur.com/485qQER.png",
+    },
+    {
+      public_id: "S8JCAWE",
+      url: "https://i.imgur.com/S8JCAWE.png",
+    },
+    {
+      public_id: "ebvuGDB",
+      url: "https://i.imgur.com/ebvuGDB.png",
+    }
+  ],
+  category: {
+    "name": "Nam",
+    "subCategory": {
+      "name": "Áo",
+      "subSubCategory": {
+        "name": "Áo sơ mi"
+      }
+    }
+  },
+  color: "Xanh",
+  size: ["S","M", "L"], 
+  stock: 1,
+  numOfReviews: 12,
+  reviews: [],
+},
+
+//4...........................................
+{
+  productID: ALD231656,
+  name: "ÁO LEN - ALD231656",
+  price: 545000,
+  description:
+    "Áo len dáng regular fit tôn dáng người mặc. Màu sắc thanh lịch dễ phối đồ.",
+  ratings: 4.5,
+  images: [
+    {
+      public_id: "ivttBn1",
+      url: "https://i.imgur.com/ivttBn1.png",
+    },
+    {
+      public_id: "Xwozhxr",
+      url: "https://i.imgur.com/Xwozhxr.png",
+    },
+    {
+      public_id: "IUPF0sF",
+      url: "https://i.imgur.com/IUPF0sF.png",
+    },
+  ],
+  category: {
+    "name": "Nam",
+    "subCategory": {
+      "name": "Áo",
+      "subSubCategory": {
+        "name": "Áo len"
+      }
+    }
+  },
+  color: "Đỏ",
+  size: ["S","M"], 
+  stock: 11,
+  numOfReviews: 112,
+  reviews: [],
+},
+
+  //5.......................................... 
+{
+  productID: QJS230161,
+  name: "QUẦN JEANS - QJS230161",
+  price: 625000,
+  description:
+    "Quần jeans dáng slimfit ôm dáng người mặc. Màu sắc trẻ trung dễ phối đồ.",
+  ratings: 4.8,
+  images: [
+    {
+      public_id: "jjPXifI",
+      url: "https://i.imgur.com/ivttBn1.png",
+    },
+    {
+      public_id: "4W6323b",
+      url: "https://i.imgur.com/4W6323b.png",
+    },
+  ],
+  category: {
+    "name": "Nam",
+    "subCategory": {
+      "name": "Quần",
+      "subSubCategory": {
+        "name": "Quần Jeans"
+      }
+    }
+  },
+  color: "Xanh",
+  size: ["M", "L"], 
+  stock: 122,
+  numOfReviews: 2671,
+  reviews: [],
+},
+
+//6.............................................
+{
+  productID: QS231535,
+  name: "QUẦN TÂY - QS231535",
+  price: 699000,
+  description:
+    "Kiểu dáng Regular Fit dễ mặc, hợp form dáng. Màu sắc và kiểu dáng trẻ trung, kiểu dáng hiện đại, dễ phối đồ.",
+  ratings: 4.1,
+  images: [
+    {
+      public_id: "DJrhCh7",
+      url: "https://i.imgur.com/DJrhCh7.png",
+    },
+    {
+      public_id: "62h2dhh",
+      url: "https://i.imgur.com/62h2dhh.png",
+    },
+  ],
+  category: {
+    "name": "Nam",
+    "subCategory": {
+      "name": "Quần",
+      "subSubCategory": {
+        "name": "Quần tây"
+      }
+    }
+  },
+  color: "Trắng",
+  size: ["S","M", "L"], 
+  stock: 1123,
+  numOfReviews: 6,
+  reviews: [],
+},
+
+//7..............................................
+{
+  productID: BELT232615,
+  name: "THẮT LƯNG - BELT232615",
+  price: 799000,
+  description: 
+    "Chất liệu: Leather (Da)",
+  ratings: 4.7,
+  images: [
+    {
+      public_id: "D4bsMSN",
+      url: "https://i.imgur.com/D4bsMSN.png",
+    },
+  ],
+  category: {
+    "name": "Nam",
+    "subCategory": {
+      "name": "Phụ kiện",
+      "subSubCategory": {
+        "name": "Thắt lưng"
+      }
+    }
+  },
+  color: "Đen",
+  size: "F", 
+  stock: 131,
+  numOfReviews: 222,
+  reviews: [],
+},
+
+//8..............................................
+{
+  productID: CV221500,
+  name: "CÀ VẠT - CV221500",
+  price: 499000,
+  description:
+    "Màu sắc và kiểu dáng trẻ trung, kiểu dáng hiện đại, dễ phối đồ.",
+  ratings: 4.9,
+  images: [
+    {
+      public_id: "ZDgWQYW",
+      url: "https://i.imgur.com/ZDgWQYW.png",
+    },
+   
+  ],
+  category: {
+    "name": "Nam",
+    "subCategory": {
+      "name": "Phụ kiện",
+      "subSubCategory": {
+        "name": "Thắt lưng"
+      }
+    }
+  },
+  color: "Xanh",
+  size: "F",
+  stock: 0,
+  numOfReviews: 56,
+  reviews: [],
+},
+
+//9..................................................
+{
+  productID: TA221475,
+  name: "TẤT - TA221475",
+  price: 39000,
+  description:
+    "Chất liệu Cotton co giãn thoải mái thoáng khí. Thiết kế Basic hiện đại, trẻ trung",
+  ratings: 5,
+  images: [
+    {
+      public_id: "DvnzzGn",
+      url: "https://i.imgur.com/DvnzzGn.png",
+    },
+    
+  ],
+  category: {
+    "name": "Nam",
+    "subCategory": {
+      "name": "Phụ kiện",
+      "subSubCategory": {
+        "name": "Thắt lưng"
+      }
+    }
+  },
+  color: "Đen",
+  size: ["S","M", "L"], 
+  stock: 500,
+  numOfReviews: 1,
+  reviews: [],
+},
+
+//10...................................................
+{
+  productID: TA221476,
+  name: "TẤT - TA221476",
+  price: 39000,
+  description:
+    "Chất liệu Cotton co giãn thoải mái thoáng khí. Thiết kế Basic hiện đại, trẻ trung",
+  ratings: 5,
+  images: [
+    {
+      public_id: "ptqq3z0",
+      url: "https://i.imgur.com/ptqq3z0.jpg",
+    },
+    {
+      public_id: "L4OgwPA",
+      url: "https://i.imgur.com/L4OgwPA.png",
+    },
+  ],
+  category: {
+    "name": "Nam",
+    "subCategory": {
+      "name": "Áo",
+      "subSubCategory": {
+        "name": "Áo sơ mi"
+      }
+    }
+  },
+  color: "Trắng",
+  size: ["S","M", "L"], 
+  stock: 50,
+  numOfReviews: 1,
+  reviews: [],
+},
+
+
 ];
 
 
