8487558394705342fbd4397ba3b052fbd853695d
Pham Tien Nam <tiennam27@gmail.com>
2024-04-14 21:44:10 +0700

    update login user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,10 @@
 
 import express from "express";
-import { registerUser } from "../controllers/authControllers.js";
-
+import {loginUser, registerUser } from "../controllers/authControllers.js";
 const router = express.Router();
+
 // Router route(dẫn) đến mục "/products" để get (nhận request) và đưa vào controller function (hàm điều khiển)
 router.route("/register").post(registerUser);
-
+router.route("/login").post(loginUser);
 export default router;
 
