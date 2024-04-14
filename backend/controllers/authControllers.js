9f719da3629ba91f8b3552c27a5c9d797bb2dce7
Pham Tien Nam <tiennam27@gmail.com>
2024-04-14 21:44:10 +0700

    update login user

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,9 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js"
 import User from "../models/user.js";
+import ErrorHandler from "../utils/errorHandler.js";
+
 // Hàm xử lý yêu cầu đăng ký người dùng
+//Register User => /api/register
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
   // Trích xuất thông tin người dùng từ req.body sử dụng destructuring
   const {userID, name, email, password, address, phone } = req.body;
@@ -15,8 +18,40 @@ export const registerUser = catchAsyncErrors(async (req, res, next) => {
     phone,
   });
 
+  const token = user.getJwtToken();
+
   // Trả về một phản hồi thành công với mã trạng thái HTTP 201
   res.status(201).json({
-    success: true,
+    token,
+  });
+});
+
+
+//Login User => /api/login
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+  // Trích xuất thông tin người dùng từ req.body sử dụng destructuring
+  const {email, password} = req.body;
+
+  if(!email || !password){
+    return next(new ErrorHandler("Vui lòng nhập email và mật khẩu", 400))
+  }
+
+  const user = await User.findOne({email}).select("+password")
+
+  if(!user){
+    return next(new ErrorHandler("email hoặc mật khẩu không đúng", 401))
+  }
+
+  const isPassWordMatched = await user.comparePassword(password)
+
+  if(!isPassWordMatched){
+    return next(new ErrorHandler("username hoặc mật khẩu không đúng", 401))
+  }
+
+  const token = user.getJwtToken(); 
+
+  // Trả về một phản hồi thành công với mã trạng thái HTTP 201
+  res.status(200).json({
+    token,
   });
 });
