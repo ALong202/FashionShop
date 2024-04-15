686765b5eeeb6a054288f164981659cfb8bc8c99
ricardotran92 <quanghung.850621@gmail.com>
2024-04-15 18:49:42 +0700

    Revert to commit c3ba337

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 import express from "express";// File nhập liệu chính của backend
 const app = express();// Tạo 1 dối tượng ứng dụng Express mới
-import cookieParser from "cookie-parser";
 import dotenv from "dotenv"; // Một thư viện của JavaScript load các biến môi trường từ tập tin '.env' vào application's runtime environment
 import { connectDatabase } from "./config/dbConnect.js"; // tự hiện khi gõ lệnh connectDatabase()
 import errorsMiddleware from "./middlewares/errors.js";
@@ -16,8 +15,6 @@ dotenv.config({ path: "backend/config/config.env" });
 // Connect với database
 connectDatabase();
 app.use(express.json());
-app.use(cookieParser());
-
 // Import tất cả các routes (đường dẫn)
 import productRoutes from "./routes/products.js";
 import authRoutes from "./routes/auth.js";
