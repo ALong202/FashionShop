27593b0c5eab0d9f0778e7dff2cfe525fec8849c
ricardotran92 <quanghung.850621@gmail.com>
2024-04-16 07:28:41 +0700

    Trang Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
+import "./App.css";
 // React router DOM quản lý routing. Router xác định các routes. Route: URL path
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './component/Home';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from "./component/Home";
 
 import Footer from "./component/layout/Footer";
 import Header from "./component/layout/Header";
 // Toast package giúp hiển thị thông báo success, error, warning... https://www.npmjs.com/package/react-hot-toast
 import { Toaster } from "react-hot-toast";
-import ProductDetails from './component/product/ProductDetails';
-import Login from './component/auth/Login'; // auto chèn khi chọn Login từ Quick Fix
+import ProductDetails from "./component/product/ProductDetails";
+import Login from "./component/auth/Login"; // auto chèn khi chọn Login từ Quick Fix
+import Register from "./component/auth/Register"; // auto chèn
 // import './custom-theme.min.css';
 
 import Cart from "./component/cart/Cart"
 import Shipping from "./component/cart/Shipping"
-import ConfirmOrder from './component/cart/ConfirmOrder';
-import PaymentMethod from './component/cart/PaymentMethod';
+import ConfirmOrder from "./component/cart/ConfirmOrder";
+import PaymentMethod from "./component/cart/PaymentMethod";
+
 
 
 
@@ -31,6 +33,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/cart" element={<Cart />} />
           {/**một số route cần có route cho ac đăng ký và chưa đăng ký */}
           <Route path="/shipping" element={<Shipping />} />
--- frontend/src/redux/api/authApi.js
+++ frontend/src/redux/api/authApi.js
@@ -2,7 +2,7 @@
 slash login là post request (mutation, thay vì query) gửi đến backend, backend xử lý và trả về response
 https://redux-toolkit.js.org/rtk-query/usage/mutations
 
-useLoginMutation được tạo ra từ endpoint login trong authApi. Endpoint này gửi 1 request POST đến /api/login với credentials (email, password) và trả về response.
+useLoginMutation là hook được tạo bởi Redux Toolkit Query (RTK Query) từ endpoint login trong authApi. Endpoint này gửi 1 request POST đến /api/login với credentials (email, password) và trả về response.
 Khi đăng nhập sẽ dùng lệnh POST thay vì GET vì dữ liệu credentials không nên hiển thị trên URL.
 */ 
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
@@ -15,6 +15,16 @@ export const authApi = createApi({
   // builder to access the query function (GET), mutations (POST, PUT, PATCH, DELETE), send requests
   // endpoints để gửi request đến backend
   endpoints: (builder) => ({ 
+    // Register mutation
+    register: builder.mutation({
+      query(body) {
+        return{
+          url: "/register",
+          method: "POST",
+          body,
+        }
+      },
+    }),    
     // mutation để login
     login: builder.mutation({
       query(body) {
@@ -29,4 +39,4 @@ export const authApi = createApi({
 })
 
 // the hook để lấy toàn bộ sản phẩm, tất cả biến Isloading-sucess-error variables
-export const { useLoginMutation } = authApi;
+export const { useLoginMutation, useRegisterMutation } = authApi;
