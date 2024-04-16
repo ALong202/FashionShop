855c8b970c672b2cab8788f81225a8ff7e9446a5
ricardotran92 <quanghung.850621@gmail.com>
2024-04-17 00:08:48 +0700

    Set up in DEV and PROD mode by nodemon

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -7,6 +7,7 @@ export const connectDatabase = () => {
 
   if (process.env.NODE_ENV === "NORMAL") DB_URI = process.env.DB_LOCAL_URI;
   if (process.env.NODE_ENV === "DEVELOPMENT") DB_URI = process.env.DB_URI;
+  if (process.env.NODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI;
 
   // phương thức connect của mongoose kết nối với URI. Nếu thành công, hàm 'then' được gọi với 1 callback nhận đối số là object 'con' chứa thông tin kết nối. Sau đó in ra với thuộc tính 'host' trong object kết nối MongoDB 'con'
   mongoose.connect(DB_URI).then((con) => {
