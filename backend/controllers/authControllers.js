c400c9c2567fe27bf3a16ffdbe2e98df90f2d6ca
Pham Tien Nam <tiennam27@gmail.com>
2024-04-16 12:23:51 +0700

    fix filter subcategory

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,7 +1,10 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js"
 import User from "../models/user.js";
+import { getResetPasswordTemplate } from "../utils/emailTemplates.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import sendToken from "../utils/sendToken.js";
+import sendEmail from "../utils/sendEmail.js";
+import crypto from "crypto";
 
 // Hàm xử lý yêu cầu đăng ký người dùng
 //Register User => /api/register
@@ -28,20 +31,71 @@ export const loginUser = catchAsyncErrors(async (req, res, next) => {
   const {email, password} = req.body;
 
   if(!email || !password){
-    return next(new ErrorHandler("Vui lòng nhập email và mật khẩu", 400))
+    return next(new ErrorHandler("Vui lòng nhập email và mật khẩu", 400));
   }
 
-  const user = await User.findOne({email}).select("+password")
+  const user = await User.findOne({email}).select("+password");
 
   if(!user){
-    return next(new ErrorHandler("email hoặc mật khẩu không đúng", 401))
+    return next(new ErrorHandler("email hoặc mật khẩu không đúng", 401));
   }
 
   const isPassWordMatched = await user.comparePassword(password)
 
   if(!isPassWordMatched){
-    return next(new ErrorHandler("username hoặc mật khẩu không đúng", 401))
+    return next(new ErrorHandler("username hoặc mật khẩu không đúng", 401));
   }
 
-  sendToken(user, 201, res)
+  sendToken(user, 201, res);
+});
+
+
+// Logout user   =>  /api/v1/logout
+export const logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    message: "Đăng Xuất",
+  });
+});
+
+
+export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
+
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHandler("Không tìm thấy email", 404));
+  }
+ 
+  
+  const resetToken = user.getResetPasswordToken(); 
+
+  await user.save();
+
+
+  const resetUrl = `${process.env.FRONTEND_URL}/api/password/reset/${resetToken}`;
+ 
+  const message = getResetPasswordTemplate(user?.name, resetUrl);
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: " FashionShop - Email lấy lại mật khẩu",
+      message,
+    });
+
+    res.status(200).json({
+      message: `Email từ: ${user.email}`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+    return next(new ErrorHandler(error?.message, 500));
+  }
 });
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,14 @@
 
 import express from "express";
-import {loginUser, registerUser } from "../controllers/authControllers.js";
+import {forgotPassword, logout, loginUser, registerUser } from "../controllers/authControllers.js";
 const router = express.Router();
 
 // Router route(dẫn) đến mục "/products" để get (nhận request) và đưa vào controller function (hàm điều khiển)
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+
+router.route("/logout").get(logout);
+
+router.route("/password/forgot").post(forgotPassword);
 export default router;
 
