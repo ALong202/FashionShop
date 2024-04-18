5285db31efc09c1a90e50a7b3e1fcd3cc2f37020
ricardotran92 <quanghung.850621@gmail.com>
2024-04-18 10:35:00 +0700

    Thêm lựa chọn màu ở trang chi tiết sản phẩm

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -7,7 +7,7 @@ export const PRODUCT_CATEGORIES = [
 export const PRODUCT_SUBCATEGORIES = {
   "Nam": ["Áo", "Quần", "Phụ kiện"],
   "Nữ": ["Áo", "Quần", "Chân váy và đầm"], 
-  "Trẻ em": ["Áo", "Quần"]
+  // "Trẻ em": ["Áo", "Quần"]
 }
 
 export const PRODUCT_SUBSUBCATEGORIES = {
@@ -16,3 +16,17 @@ export const PRODUCT_SUBSUBCATEGORIES = {
   "Phụ kiện": ["Cà vạt", "Tất", "Thắt lưng"],
   "Chân váy và đầm": ["Chân váy ngắn", "Chân váy dài", "Đầm dự tiệc"]
 }
+
+export const colorMap = {
+  "Đỏ": "#FF0000",
+  "Hồng": "#FFC0CB",
+  "Sọc": "#000000", // Mã màu cho "Sọc" phụ thuộc vào màu của các sọc
+  "Vàng": "#FFFF00",
+  "Xám": "#808080",
+  "Trắng": "#FFFFFF",
+  "Nâu": "#A52A2A",
+  "Cam": "#FFA500",
+  "Đen": "#000000",
+  "Họa tiết": "#000000", // Mã màu cho "Họa tiết" phụ thuộc vào màu của họa tiết
+  "Xanh": "#0000FF"
+};
