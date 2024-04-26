3136627146e9f754a43bcef60f2f547dafaede07
ricardotran92 <quanghung.850621@gmail.com>
2024-04-26 16:35:45 +0700

    Cấu trúc lại để chuẩn bhi5 chia ra user và admin.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,39 +3,23 @@ container-fluid sẽ chiếm 100% chiều rộng ở tất cả breakpoint, còn
 */
 import "./App.css";
 // React router DOM quản lý routing. Router xác định các routes. Route: URL path
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from "./component/Home";
+import { BrowserRouter as Router, Routes} from "react-router-dom"
 
-import Footer from "./component/layout/Footer";
-import Header from "./component/layout/Header";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 // Toast package giúp hiển thị thông báo success, error, warning... https://www.npmjs.com/package/react-hot-toast
 import { Toaster } from "react-hot-toast";
 import { ToastContainer } from "react-toastify"; 
 import "react-toastify/dist/ReactToastify.css"; 
-import ProductDetails from "./component/product/ProductDetails";
-import Login from "./component/auth/Login"; // auto chèn khi chọn Login từ Quick Fix
-import Register from "./component/auth/Register"; // auto chèn
-import Profile from "./component/user/Profile"; // auto chèn
-import UpdateProfile from "./component/user/UpdateProfile";
-import ProtectedRoute from "./component/auth/ProtectedRoute";
-import UploadAvatar from "./component/user/UploadAvatar";
-import UpdatePassword from "./component/user/UpdatePassword";
-// import './custom-theme.min.css';
-import Cart from "./component/cart/Cart"
-import Shipping from "./component/cart/Shipping"
-import ConfirmOrder from "./component/cart/ConfirmOrder";
-import PaymentMethod from "./component/cart/PaymentMethod";
-import ForgotPassword from "./component/auth/ForgotPassword";
-import ResetPassword from "./component/auth/ResetPassword";
-import MyOrders from "./component/order/MyOrders";
-import OrderDetails from "./component/order/OrderDetails";
-import Invoice from "./component/invoice/Invoice";
-import Top from "./component/layout/Top";
-
+import Top from "./components/layout/Top";
+import useUserRoutes from "./components/routes/userRoutes";
 
 
 
 function App() {
+
+  const userRoutes = useUserRoutes();
+
   return (
     <Router>
       <div className="App">
@@ -48,104 +32,14 @@ function App() {
         hideProgressBar={true}
         draggable
       /> 
-      {/* Page header từ ./component/layout/Footer */}
+      {/* Page header từ ./components/layout/Footer */}
       <Header/>
+
+
+
       <div className="container-fluid">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-
-          <Route path="/password/forgot" element={<ForgotPassword />} />
-          <Route path="/password/reset/:token" element={<ResetPassword />} />
-
-          {/* <Route path="/me/profile" element={<Profile />} /> 
-          <Route path="/me/update_profile" element={<UpdateProfile />} />*/}
-          <Route 
-            path="/me/profile" 
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            } 
-          />
-
-          <Route
-            path="/me/update_profile"
-              element={
-              <ProtectedRoute>
-                <UpdateProfile/>
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/me/upload_avatar"
-              element={
-                <ProtectedRoute>
-                  <UploadAvatar/>
-                </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/me/update_password"
-              element={
-                <ProtectedRoute>
-                  <UpdatePassword/>
-                </ProtectedRoute>
-            }
-          />
-          <Route path="/cart" element={<Cart />} />
-          {/**một số route cần có route cho ac đăng ký và chưa đăng ký */}
-          <Route
-            path="/shipping" 
-              element={
-                <ProtectedRoute>
-                  <Shipping />
-                </ProtectedRoute>
-                } 
-          />
-          <Route
-            path="/confirm_order" 
-              element={
-                <ProtectedRoute>
-                  <ConfirmOrder />
-                </ProtectedRoute>
-                } 
-          />
-          <Route
-            path="/payment_method" 
-              element={
-                <ProtectedRoute>
-                  <PaymentMethod />
-                </ProtectedRoute>
-                } 
-          />
-          <Route
-            path="/me/orders" 
-              element={
-                <ProtectedRoute>
-                  <MyOrders />
-                </ProtectedRoute>
-                } 
-          />
-          <Route
-            path="/me/orders/:id" 
-              element={
-                <ProtectedRoute>
-                  <OrderDetails />
-                </ProtectedRoute>
-                } 
-          />
-          <Route
-            path="/invoice/orders/:id" 
-              element={
-                <ProtectedRoute>
-                  <Invoice />
-                </ProtectedRoute>
-                } 
-          />
+          {userRoutes}
         </Routes>
 
       </div>
@@ -153,7 +47,7 @@ function App() {
       {/* Nút Scroll to Top */}
       <Top/>
               
-      {/* Page Footer từ ./component/layout/Header*/}
+      {/* Page Footer từ ./components/layout/Header*/}
       <Footer/>
     </div>
     </Router>
