1c0b11aa3bc8bf9f485d6daf0e1c1fba969a3e7c
pmquoc-uit <pmquoc.uit@gmail.com>
2024-07-19 17:31:09 +0700

    update .gitignore for docker

---
+++ docker-run.ps1
@@ -0,0 +1,10 @@
+# # Set environment variables
+# $env:FRONTEND_URL = "http://localhost:3000"
+# $env:FRONTEND_PUB_URL = "http://localhost:3000"
+# $env:FRONTEND_PROD_URL = "https://a7f1-14-237-10-196.ngrok-free.app"
+# $env:BACKEND_URL = "http://localhost:3001"
+# $env:BACKEND_PUB_URL = "https://a7f1-14-237-10-196.ngrok-free.app"
+# $env:BACKEND_PROD_URL = "https://a7f1-14-237-10-196.ngrok-free.app"
+
+# Run Docker container with specified environment file
+docker run -p 3001:3001 fashionshop:180724
